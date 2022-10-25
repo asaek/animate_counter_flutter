f38c6dfe1487d33e15c896319bbecd75c3d10334
Asaek <Asaek60@gmail.com>
2022-10-25 18:58:19 -0500

    Moviendo el boton del contador

--- lib/widgets/animate_custom.dart
+++ lib/widgets/animate_custom.dart
@@ -11,6 +11,8 @@ class AnimateWidget extends StatefulWidget {
 
 class _AnimateWidgetState extends State<AnimateWidget> {
   int _contador = 0;
+  Alignment _dragAligment = Alignment.center;
+
   @override
   void initState() {
     super.initState();
@@ -24,72 +26,102 @@ class _AnimateWidgetState extends State<AnimateWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 80,
-      width: 240,
-      // color: Colors.orangeAccent,
+    const maxHeight = 80.0;
+    const maxWidth = 300.0;
+    return ConstrainedBox(
+      constraints: const BoxConstraints(maxWidth: maxWidth),
       child: Material(
         color: const Color(0xFF282828),
         borderRadius: BorderRadius.circular(70),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            IconButton(
-              splashRadius: _splashRadius,
-              icon: const Icon(
-                Icons.remove,
-                color: Colors.white,
-              ),
-              onPressed: () {
-                setState(() {
-                  _contador--;
-                });
-                print('Wee apunta toda tu energia a kyary');
-              },
-            ),
-            GestureDetector(
-              onPanUpdate: (details) {
-                print(details.delta.dx);
-                print(details.delta.dy);
-              },
-              child: SizedBox(
-                height: 80,
-                width: 70,
-                child: FloatingActionButton(
-                  backgroundColor: const Color(0xFF3B3B3B),
-                  child: FittedBox(
-                    child: Text(
-                      _contador.toString(),
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w400,
-                        fontSize: 18,
-                      ),
+        child: SizedBox(
+          height: maxHeight,
+          child: Stack(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                          splashRadius: _splashRadius,
+                          icon: const Icon(
+                            Icons.remove,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _contador--;
+                            });
+                            print('Wee apunta toda tu energia a kyary');
+                          },
+                        ),
+                        IconButton(
+                          splashRadius: _splashRadius,
+                          icon: const Icon(
+                            Icons.add,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _contador++;
+                            });
+                            print('Wee hechale ganas kyary esta ahi');
+                          },
+                        ),
+                      ],
                     ),
                   ),
-                  onPressed: () {
+                ],
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: GestureDetector(
+                  onPanUpdate: (details) {
+                    print(details.delta.dx);
+                    print(details.delta.dy);
+
                     setState(() {
-                      _contador++;
+                      // _dragAligment += Alignment(
+                      //   details.delta.dx / (constraints.maxWidth / 2),
+                      //   0,
+                      // );
+                      if (_dragAligment.x > 1) {
+                        _dragAligment = const Alignment(1.0, 0.0);
+                      } else if (_dragAligment.x < -1) {
+                        _dragAligment = const Alignment(-1.0, 0.0);
+                      }
                     });
-                    print('Wee hechale ganas kyary esta ahi');
                   },
+                  child: SizedBox(
+                    height: 80,
+                    width: 70,
+                    child: FloatingActionButton(
+                      backgroundColor: const Color(0xFF3B3B3B),
+                      child: FittedBox(
+                        child: Text(
+                          _contador.toString(),
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _contador++;
+                        });
+                        print('Wee hechale ganas kyary esta ahi');
+                      },
+                    ),
+                  ),
                 ),
               ),
-            ),
-            IconButton(
-              splashRadius: _splashRadius,
-              icon: const Icon(
-                Icons.add,
-                color: Colors.white,
-              ),
-              onPressed: () {
-                setState(() {
-                  _contador++;
-                });
-                print('Wee hechale ganas kyary esta ahi');
-              },
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
